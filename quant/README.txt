ff0fa62e7c73de1d2ade76429b0b513f516bf87b
Jyotishkar Dey <jyotishkardey@gmail.com>
2024-12-28 18:21:44 +0000

    Initial Submit

--- quant/README.txt
+++ quant/README.txt
@@ -1,1 +1,4 @@
 Fix error regarding https://ta-lib.github.io/ta-lib-python/install.html
+
+YFinace Documentation
+https://algotrading101.com/learn/yahoo-finance-api-guide/
--- quant/src/buy_nse_500.py
+++ quant/src/buy_nse_500.py
@@ -4,7 +4,7 @@ import talib
 
 # Function to check if the stock is in uptrend for the last 15 days
 def is_uptrend(data):
-    return data['Close'][-1] > data['Close'][0]
+    return data['Close'].iloc[-1] > data['Close'].iloc[0]
 
 # Function to check RSI
 def check_rsi(data):
@@ -17,12 +17,13 @@ def check_macd(data):
     return macd[-1] > macdsignal[-1]
 
 # Get list of Nifty 500 stocks (assuming this list is available)
-nifty_500_stocks = ['RELIANCE.NS', 'TCS.NS', 'HDFCBANK.NS']  # Add all Nifty 500 stocks here
+nifty_500_stocks = ['RELIANCE.NS']  # Add all Nifty 500 stocks here
 
 # Filter stocks based on criteria
 filtered_stocks = []
 for stock in nifty_500_stocks:
-    data = yf.download(stock, period='15d', interval='1d')
+    ticker =  yf.Ticker(stock)
+    data = ticker.history(period='1mo')
     if is_uptrend(data) and check_rsi(data) and check_macd(data):
         filtered_stocks.append(stock)
 
