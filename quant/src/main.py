13713c12992b64fa8ae2e1b8a59722462d0b9a87
jyotishkardey <jyotishkardey@gmail.com>
2024-12-29 00:54:49 +0000

    Moving average

--- quant/src/main.py
+++ quant/src/main.py
@@ -23,10 +23,32 @@ def check_macd(data):
 
 def check_adx(data):
     data['ADX'] = talib.ADX(data['High'], data['Low'], data['Close'], timeperiod=14)
-    return data['ADX'].iloc[-1] > 20
+    return data['ADX'].iloc[-1] > 15
+
+def is_obv_increasing(data):
+    # Calculate OBV
+    data['OBV'] = talib.OBV(data['Close'], data['Volume'])
+
+    # Calculate the difference in OBV
+    data['OBV_Diff'] = data['OBV'].diff()
+
+    # Get the last 5 elements
+    last_few =data['OBV_Diff'][-1:]
+    # Check if the OBV is increasing (most recent difference is positive)
+    return all(x > 0 for x in last_few) 
+
+def compare_moving_average(data):
+    # Calculate the 50-day and 200-day moving averages
+    data['50_MA'] = data['Close'].rolling(window=50).mean()
+    data['200_MA'] = data['Close'].rolling(window=200).mean()
+    # Check if the 50-day MA is greater than the 200-day MA
+    if data['50_MA'].iloc[-1] > data['200_MA'].iloc[-1]:
+        return True
+    else:
+        return False
 
 def factor_momentum(data):
-    return is_uptrend(data) and check_rsi(data) and check_macd(data) and check_adx(data)
+    return is_uptrend(data) and check_rsi(data) and check_macd(data) and check_adx(data) and is_obv_increasing(data) and compare_moving_average(data)
 
 # Filter stocks based on criteria
 def main():
