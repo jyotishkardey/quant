08d4a2ec5232cd82aa4a72e11464370fea635ef7
jyotishkardey <jyotishkardey@gmail.com>
2024-12-30 15:03:41 +0000

    Enhancement of Mutual Fund prototype analyzer

--- quant/src/settings.py
+++ quant/src/settings.py
@@ -1,3 +1,7 @@
 output_file = "/home/jyotishkardey/quant/output.csv"
 input_data='/home/jyotishkardey/quant/ind_nifty500list.csv'
 enable_whatsapp_Notification = False
+
+#Mutual Funds
+fund_names = ['Motilal Flexicap']
+mutual_fund_ticker_symbols = ['0P0001BA79.BO']
