18a7f2c8c792f96900628a4148a7b14c520de973
jyotishkardey <jyotishkardey@gmail.com>
2025-01-06 11:51:41 +0000

    Store the output in put dir

--- quant/src_news/settings.py
+++ quant/src_news/settings.py
@@ -2,6 +2,6 @@
 url = "https://news.google.com/rss/search?q=Indian+stock+market&hl=en-IN&gl=IN&ceid=IN:en"
 
 # Define the file name
-file_name = "recent_articles.txt"
+file_name = "out/recent_articles.txt"
 
 enable_file_dump = True
