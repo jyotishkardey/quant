1e5f208064ac97287576218f30863e180c522e36
jyotishkardey <jyotishkardey@gmail.com>
2025-01-06 11:12:47 +0000

    Miscellaneous Changes

--- quant/src_news/news_analyzer_main.py
+++ quant/src_news/news_analyzer_main.py
@@ -1,33 +1,50 @@
 import feedparser
 from datetime import datetime, timedelta
+from settings import *
 
-# Define the URL for Google News RSS feed
-url = "https://news.google.com/rss/search?q=Indian+stock+market&hl=en-IN&gl=IN&ceid=IN:en"
 
-# Parse the RSS feed
-feed = feedparser.parse(url)
+def dump_to_file(article):
+    if dump_to_file == False:
+        return
 
-# Get the current date and time
-current_date = datetime.now()
+    # Write the recent articles to a file
+    with open(file_name, "w", encoding="utf-8") as file:
+        file.write(f"Title: {article['title']}\n")
+        file.write(f"Link: {article['link']}\n")
+        file.write(f"Published: {article['published']}\n\n")
 
-# Calculate the date 5 days ago
-five_days_ago = current_date - timedelta(days=5)
 
-# Filter articles that are less than 5 days old
-recent_articles = []
-for entry in feed.entries:
-    article_date = datetime(*entry.published_parsed[:6])
-    if article_date > five_days_ago:
-        recent_articles.append({
-            "title": entry.title,
-            "link": entry.link,
-            "published": article_date
-        })
 
-# Print the recent articles
-for article in recent_articles:
-    print(f"Title: {article['title']}\nLink: {article['link']}\nPublished: {article['published']}\n")
+def main():
+    # Parse the RSS feed
+    feed = feedparser.parse(url)
 
-# Example output when articles are not found
-if not recent_articles:
-    print("No recent articles found that are less than 5 days old.")
+    # Get the current date and time
+    current_date = datetime.now()
+
+    # Calculate the date 5 days ago
+    five_days_ago = current_date - timedelta(days=5)
+
+    # Filter articles that are less than 5 days old
+    recent_articles = []
+    for entry in feed.entries:
+        article_date = datetime(*entry.published_parsed[:6])
+        if article_date > five_days_ago:
+            recent_articles.append({
+                "title": entry.title,
+                "link": entry.link,
+                "published": article_date
+            })
+
+    # Print the recent articles
+    for article in recent_articles:
+        print(f"Title: {article['title']}\nLink: {article['link']}\nPublished: {article['published']}\n")
+        dump_to_file(article)
+    
+    # Example output when articles are not found
+    if not recent_articles:
+        print("No recent articles found that are less than 5 days old.")
+
+
+
+main()
---
+++ quant/src_news/settings.py
@@ -0,0 +1,7 @@
+# Define the URL for Google News RSS feed
+url = "https://news.google.com/rss/search?q=Indian+stock+market&hl=en-IN&gl=IN&ceid=IN:en"
+
+# Define the file name
+file_name = "recent_articles.txt"
+
+dump_to_file = True
