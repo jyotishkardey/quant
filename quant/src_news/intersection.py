aaed49117fd88af480e411bffe2413d9d98c8f72
jyotishkardey <jyotishkardey@gmail.com>
2025-01-07 05:53:33 +0000

    Intersection.py was broken.Fixed this

--- quant/src_news/intersection.py
+++ quant/src_news/intersection.py
@@ -1,18 +1,28 @@
 import pandas as pd 
 
 analysed_stocks_file_path = "../stoks_analysis.csv"
-trending_stocks_file_path = "out/recent_articles.txt2025-01-06"
+trending_stocks_file_path = "out/trending_stocks_output_file.csv"
 
 def find_intersection():
-    stocks_df = pd.read_csv(analysed_stocks_file_path)
-    trending_df = pd.read_csv(trending_stocks_file_path)
-    # Find intersection
-    intersection = stocks_df[df['Stocks'].isin(df['Stocks'])]
+    import pandas as pd
 
-    print("==================INTERSECTION===========")
-    print(intersection)
+    # Load the CSV files
+    file1 = analysed_stocks_file_path
+    file2 = trending_stocks_file_path
 
+    # Read the CSV files into DataFrames
+    df1 = pd.read_csv(file1)
+    df2 = pd.read_csv(file2)
 
+    # Specify the columns you want to compare
+    column1 = 'Stocks'
+    column2 = 'Stocks'
+
+    # Find common values using intersection
+    common_values = pd.Series(list(set(df1[column1]).intersection(set(df2[column2]))))
+
+    # Print the common values
+    print(common_values)
 
 
 find_intersection()
