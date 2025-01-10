703c34ab31af22f79482da3849b49a442cdddc58
jyotishkardey <jyotishkardey@gmail.com>
2025-01-10 07:47:00 +0000

    [WOKING][CHECKPOINT] MF CLI

--- quant/src_ai/cli_mutual_fund.py
+++ quant/src_ai/cli_mutual_fund.py
@@ -1,8 +1,25 @@
 #pip install import-ipynb
 import import_ipynb
 import mutual_fund_analyzer
+import shutil
+import os
 
+def delete_old_memory():
+    # Path to the directory you want to delete
+    folder_path = 'tmp'
 
+    # Check if the folder exists
+    if os.path.exists(folder_path):
+        # Delete the folder and its contents
+        shutil.rmtree(folder_path)
+        print(f"Folder '{folder_path}' has been deleted.")
+    else:
+        print(f"Folder '{folder_path}' does not exist.")
+
+#Delet old memory before training. So that it works on fresh data
+delete_old_memory()
+
+#Train Model
 mutual_fund_analyzer.csv_agent.run("Analyze fund_analysis.csv and give me names of 3 funds which gave greater return than NIFTY Total Market most of the times."
         "If no fund outperformed tell that top 3 funds does not exits since no fund  gave greater return than NIFTY Total Market",
  stream=True)
