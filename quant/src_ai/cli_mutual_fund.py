e8558e98705d7fbb6e27f168bfca6984e59f93ed
jyotishkardey <jyotishkardey@gmail.com>
2025-01-10 07:39:37 +0000

    WORKING Mutual Fund CLI application

--- quant/src_ai/cli_mutual_fund.py
+++ quant/src_ai/cli_mutual_fund.py
@@ -1,6 +1,11 @@
 #pip install import-ipynb
 import import_ipynb
 import mutual_fund_analyzer
-result = mutual_fund_analyzer.csv_agent.print_response("Analyze fund_analysis.csv and give me names of 3 funds which gave greater return than NIFTY Total Market most of the times."
+
+
+mutual_fund_analyzer.csv_agent.run("Analyze fund_analysis.csv and give me names of 3 funds which gave greater return than NIFTY Total Market most of the times."
         "If no fund outperformed tell that top 3 funds does not exits since no fund  gave greater return than NIFTY Total Market",
  stream=True)
+
+
+mutual_fund_analyzer.csv_agent.cli_app()
