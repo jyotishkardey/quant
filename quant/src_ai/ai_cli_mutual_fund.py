43715552dda162180380d7eae3e59252050454d4
jyotishkardey <jyotishkardey@gmail.com>
2025-01-12 13:02:35 +0000

    renamed:    cli_mutual_fund.py -> ai_cli_mutual_fund.py         renamed:    mutual_fund_analyzer.ipynb -> ai_mutual_fund_analyzer.ipynb         renamed:    news_analyzer.ipynb -> ai_news_analyzer.ipynb         new file:   ai_sentiment_analyzer.ipynb         renamed:    settings.py -> ai_settings.py         renamed:    stocks_fundamental_recommendations_analyzer.ipynb -> ai_stocks_fundamental_recommendations_analyzer.ipynb         new file:   ai_swot_analyzer.ipynb         deleted:    sentiment_analyzer.ipynb         deleted:    swot_analyzer.ipynb

--- quant/src_ai/ai_cli_mutual_fund.py
+++ quant/src_ai/ai_cli_mutual_fund.py
@@ -1,8 +1,21 @@
 #pip install import-ipynb
 import import_ipynb
-import mutual_fund_analyzer
 import shutil
 import os
+
+#Our Modules
+import ai_mutual_fund_analyzer
+import ai_settings
+
+#Import quant module
+import sys
+sys.path.append(os.path.abspath("../"))
+sys.path.append(os.path.abspath("../src_quant"))
+import src_quant.quant_main as quant_main
+import src_quant.quant_csv_utils as quant_csv_utils
+
+#import main
+quant_main.analyze_mutual_funds()
 
 def delete_old_memory():
     # Path to the directory you want to delete
@@ -16,13 +29,15 @@ def delete_old_memory():
     else:
         print(f"Folder '{folder_path}' does not exist.")
 
+quant_main.analyze_mutual_funds()
+
 #Delet old memory before training. So that it works on fresh data
 delete_old_memory()
 
 #Train Model
-mutual_fund_analyzer.csv_agent.run("Analyze fund_analysis.csv and give me names of 3 funds which gave greater return than NIFTY Total Market most of the times."
+ai_mutual_fund_analyzer.csv_agent.run("Analyze fund_analysis.csv and give me names of 3 funds which gave greater return than NIFTY Total Market most of the times."
         "If no fund outperformed tell that top 3 funds does not exits since no fund  gave greater return than NIFTY Total Market",
  stream=True)
 
 
-mutual_fund_analyzer.csv_agent.cli_app()
+ai_mutual_fund_analyzer.csv_agent.cli_app()
